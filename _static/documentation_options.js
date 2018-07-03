0d1b7add848181b2810e3cfa19583abb759a81ef
Scriptim <Scriptim@gmx.de>
2018-07-03 22:23:39 +0200

    Build documentation for 1.2.0rc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.0rc',
+    VERSION: '1.2.0rc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
